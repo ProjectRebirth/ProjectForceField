8864a3fad395a5f52895fd6050b481efdb324f58
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-09-05 21:51:25 -0400

    UnityChan! camera controls

---
+++ ProjectForceField/Assets/Scripts/Animations/UnityChanAnim.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityChanAnim : MonoBehaviour {
+	Animator anim;
+	WalkMechanics walkMechanics;
+
+	void Start() {
+		anim = GetComponent<Animator> ();
+		walkMechanics = GetComponent<WalkMechanics> ();
+	}
+
+	void Update() {
+		anim.SetBool ("isWalking", walkMechanics.getIsWalking ());
+		anim.SetBool ("isRunning", walkMechanics.getIsRunning ());
+	}
+}
---
+++ ProjectForceField/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform targetFollow;
+	public float verticalSensitivity = 5;
+	public float horizontalSensitivity = 5;
+
+	Vector3 offset;
+	float distanceFromTarget;
+
+
+	void Start() {
+		offset = this.transform.position - targetFollow.position;
+		distanceFromTarget = Vector3.Magnitude (offset);
+
+	}
+
+	void Update() {
+		print (distanceFromTarget);
+		transform.position = -transform.forward * distanceFromTarget + targetFollow.position;
+		resetZRotation ();
+	}
+
+	void resetZRotation() {
+		transform.rotation = Quaternion.Euler (transform.eulerAngles.x, transform.eulerAngles.y, 0);
+	}
+
+	public void cameraRotateHorizontal(float horizontalInput) {
+
+		transform.Rotate (new Vector3 (0, horizontalInput * horizontalSensitivity * Time.deltaTime, 0)); 
+	}
+
+	public void cameraRotateVertical(float verticalInput) {
+		transform.Rotate (new Vector3 (-verticalInput * verticalSensitivity * Time.deltaTime, 0, 0));
+	}
+}
